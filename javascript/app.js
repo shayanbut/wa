9cdd1fd458a557943c09c5c6c020db2e35fd7fdf
shayanbut <shayanbutt493@gmail.com>
2024-07-13 12:27:27 +0500

    Add files via upload
    tera ghar jain ga

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+// var number = prompt( "please emter a number");
+
+// if( number % 2 == 0){
+//     console.log(number+"even");
+// }
+// else{
+//     console.log(number+"odd");
+// }
+
+let a = "My Name Is Rehmat Ali Shah";
+let b = prompt("Type includes name");
+let check = a.includes(b);
+console.log(check);
